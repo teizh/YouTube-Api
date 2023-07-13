ca0c2c6907c9e3e1963af8452e8d06c22addcae7
Teizh <semazhainakov@gmail.com>
2023-07-13 23:04:42 +0200

    Initial

---
+++ app/src/main/java/com/example/youtube_api/data/remote/BuildConfig.kt
@@ -0,0 +1,1 @@
+package com.example.youtube_api.data.remoteclass BuildConfig {    companion object {     const val API_KEY = "AIzaSyDIUttqi6h6E0-YeHRl7pEb7cem7y0qhNY"      // const val API_KEY = "AIzaSyBHMFNBwXJZBqLCKt0P-Nd_3K3DyVHjYCs"        const val BASE_URL = "https://www.googleapis.com/youtube/v3/"    }}
